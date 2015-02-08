b3c76750d3b91f3395c9186d22f3d3af7deafea5
pravin <pravinkmrp@gmail.com>
2015-02-08 17:42:14 -0500

    New Java files

---
+++ hello/src/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+	Date ltime;
+	ltime = new Date();
+	System.out.println(ltime);
+        System.out.println(greeter.sayHello());
+    }
+}

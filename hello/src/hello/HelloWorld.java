729885be4e1c305dd163c88ff83e6ca06a2c632a
pravin <pravinkmrp@gmail.com>
2015-02-08 19:18:16 -0500

    Updated print 1

--- hello/src/hello/HelloWorld.java
+++ hello/src/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         Greeter greeter = new Greeter();
 	Date ltime;
 	ltime = new Date();
-	System.out.println("LocalTime:" + ltime);
+	System.out.println(" My LocalTime:" + ltime);
         System.out.println(greeter.sayHello());
     }
 }
